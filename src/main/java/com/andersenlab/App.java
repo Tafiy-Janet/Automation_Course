feb7b5165dc09ac55e013599311696d4fb761631
Admin <janet.tafiy@gmail.com>
2022-10-17 17:30:20 +0300

    Created Maven project

---
+++ src/main/java/com/andersenlab/App.java
@@ -0,0 +1,13 @@
+package com.andersenlab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
