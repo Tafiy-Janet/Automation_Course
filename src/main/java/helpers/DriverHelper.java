6906162e4128aa8d37c17f9765747f20c1fab59c
Admin <janet.tafiy@gmail.com>
2022-10-25 00:03:35 +0300

    Added disposeDriver() method

--- src/main/java/helpers/DriverHelper.java
+++ src/main/java/helpers/DriverHelper.java
@@ -14,10 +14,10 @@ public class DriverHelper {
     private static WebDriver get() {
         WebDriverManager.chromedriver().setup();
         driver = WebDriverFactory.getWebDriver(WebDriverFactory.Browser.CHROME);
+        driver.get(PropertyReader.readURL());
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.get(PropertyReader.readURL());
         getShadowRoot();
         acceptAlert();
         return driver;
@@ -38,18 +38,19 @@ public class DriverHelper {
 
     public static void acceptAlert() {
         By alert = By.xpath("//div[@class='disclaimer']");
+        By acceptButton = By.xpath("//button[@class='agree']");
         if (driver.findElement(alert).isDisplayed()) {
-            By acceptButton = By.xpath("//button[@class='agree']");
             driver.findElement(acceptButton).click();
         }
     }
 
     public static void getShadowRoot() {
         By shadowSelector = By.tagName("getsitecontrol-widget");
+        By rootSelector = By.className("secondary");
         if (isElementPresent(shadowSelector)) {
             WebElement shadowHost = driver.findElement(shadowSelector);
             SearchContext shadowRoot = shadowHost.getShadowRoot();
-            shadowRoot.findElement(By.className("secondary")).click();
+            shadowRoot.findElement(rootSelector).click();
         }
     }
 
