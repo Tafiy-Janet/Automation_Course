2ebd070c45ba5b5ee89e5265316738b361e94df2
Admin <janet.tafiy@gmail.com>
2022-11-04 00:05:04 +0200

    Reworked methods to handle UI flakiness

--- src/main/java/helpers/TabManagement.java
+++ src/main/java/helpers/TabManagement.java
@@ -3,13 +3,10 @@ package helpers;
 import java.util.ArrayList;
 
 public class TabManagement extends DriverBase {
-    private static ArrayList<String> allWindowTabs = new ArrayList<>(getDriver().getWindowHandles());
 
     public static void switchToNewTab() {
+        ArrayList<String> allWindowTabs = new ArrayList<>(getDriver().getWindowHandles());
         getDriver().switchTo().window(allWindowTabs.get(1));
     }
 
-    public static void switchToMainTab() {
-        getDriver().switchTo().window(allWindowTabs.get(0));
-    }
 }
--- src/main/java/helpers/WaitHelper.java
+++ src/main/java/helpers/WaitHelper.java
@@ -5,15 +5,40 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
+import java.time.Duration;
+import java.util.List;
 
 
 public class WaitHelper extends DriverBase {
 
     private static WebDriverWait wait = new WebDriverWait(getDriver(), DEFAULT_TIMEOUT_SECONDS);
-    public static void waitAndClick(WebElement locator) {
-        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+
+    public static boolean waitAndClick(WebElement locator) {
+        var retries = 0;
+        while(retries <= DEFAULT_RETRIES) {
+            try {
+                wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+                return true;
+            }catch (Exception e){
+                retries++;
+            }
+        }
+        return  false;
+    }
+    public static boolean waitAndClick(List<WebElement> locators) {
+        var retries = 0;
+        while(retries <= DEFAULT_RETRIES) {
+            try {
+                for (WebElement clickable: locators) {
+                    wait.until(ExpectedConditions.elementToBeClickable(clickable)).click();
+                }
+                return true;
+            }catch (Exception e){
+                retries++;
+            }
+        }
+        return false;
     }
     public static WebElement waitClickable(WebElement locator) {
         return wait.until(ExpectedConditions.elementToBeClickable(locator));
@@ -26,8 +51,9 @@ public class WaitHelper extends DriverBase {
     public static void implicitWait() {
         getDriver().manage().timeouts().implicitlyWait(DEFAULT_TIMEOUT_SECONDS);
     }
+
     public static void JSWaiter() {
-        new WebDriverWait(getDriver(),Duration.ofSeconds(10)).until((ExpectedCondition<Boolean>) wd -> ((JavascriptExecutor) wd)
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until((ExpectedCondition<Boolean>) wd -> ((JavascriptExecutor) wd)
                 .executeScript("return document.readyState")
                 .equals("complete"));
     }
